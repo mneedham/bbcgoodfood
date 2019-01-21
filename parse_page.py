4ee01d5d7b7655a8099e572ba2ee11d96e430b6d
Mark Needham <m.h.needham@gmail.com>
2019-01-21 21:12:02 +0000

    recipes as json

--- parse_page.py
+++ parse_page.py
@@ -1,9 +1,16 @@
 import re
 import demjson
 from bs4 import BeautifulSoup
+import glob
 
-file = open("data/raw/2852672.html").read()
-soup = BeautifulSoup(file, 'html.parser')
-ingredients_cell = [entry for entry in soup.find_all("script") if "ingredients" in entry.text][0]
-blah = re.findall("permutive.addon\(\"web\",(.*)\);", ingredients_cell.text.strip())
-print(demjson.decode(blah[0])["page"]["recipe"]["ingredients"])
+with open("stream_all.json", "w") as json_file:
+    for file_path in glob.glob("lju_recipes/*.html"):
+        with open(file_path) as file:
+            soup = BeautifulSoup(file.read(), 'html.parser')
+            ingredients_cell = [entry for entry in soup.find_all("script") if "ingredients" in entry.text][0]
+            js_object = re.findall("permutive.addon\(\"web\",(.*)\);", ingredients_cell.text.strip())
+            if len(js_object) > 0:
+                try:
+                    print(demjson.decode(js_object[0]), file=json_file)
+                except demjson.JSONDecodeError as error:
+                    print(error, js_object)
